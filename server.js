91411a05f2911a2640125c5e03b1792ab0992515
Michael Igoe <michael.igoe@cdetb.ie>
2018-05-16 17:21:23 +0100

    Changes to JS version in server.js

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+const port = 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
