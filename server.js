863ff292fd4e35925ef2b6078f8c52fd5d94e6fe
Mike Igoe <mikeigoe@gmail.com>
2018-05-21 21:51:34 +0100

    Working on header generation

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const expressOpenApi = require('express-openapi');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const CryptoJS = require('crypto-js');
 
 const app = express();
 
@@ -9,13 +11,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+//const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
-mongoose
-  .connect(db)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+//mongoose
+//.connect(db)
+//.then(() => console.log('MongoDB connected'))
+//.catch(err => console.log(err));
+
+var password = 'af4eb226';
+var data = [{ id: 1 }, { id: 2 }];
+
+// Encrypt
+var ciphertext = CryptoJS.MD5.encrypt(JSON.stringify(data), password);
+
+// Decrypt
+var bytes = CryptoJS.MD5.decrypt(ciphertext.toString(), password);
+var decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Latin1));
+
+console.log(decryptedData);
 
 const port = 5000;
 
