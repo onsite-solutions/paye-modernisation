2c6b5379952ba0c977890734d57345936fc20aea
Mike Igoe <mikeigoe@gmail.com>
2018-05-22 06:53:23 +0100

    Added certs

---
+++ digital-certs/certs.js
@@ -0,0 +1,12 @@
+export const Certs = [
+  {
+    name: 'PIT Access',
+    fileName: '999963666.p12',
+    password: '01e06536'
+  },
+  {
+    name: 'Fake company',
+    fileName: '999963666.p12',
+    password: 'af4eb226'
+  }
+];
