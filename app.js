9d16571142aea337434e239c0e2d2748e9643ba0
Michael Igoe <michael.igoe@cdetb.ie>
2018-05-22 17:41:37 +0100

    Simplified process with built-in crypto, removed unused npm packages

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
-const md5 = require('md5');
-const btoa = require('btoa');
+const crypto = require('crypto');
+
+var password = 'Password123';
+
+console.log(`password: ${password}`);
+
+// Use the node crypto module to get the md5 hash of the password
+
+var hashedPassword = crypto
+  .createHash('md5')
+  .update(password)
+  .digest('base64');
+
+console.log(`hashed: ${hashedPassword}`);
+
+/* NOTE: The above code has achieved the same as the functions commented out below */
 
 /*
-const getHashedPassword = require('./cryptography/getHashedPassword')
-  .getHashedPassword;
-
-var password = 'password123';
-
-console.log(getHashedPassword(password));
-
-
-function hex2a(hexx) {
-  var hex = hexx.toString(); //force conversion
-  var str = '';
-  for (var i = 0; i < hex.length; i += 2)
-    str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-  return str;
-}
-*/
-
 // src: https://stackoverflow.com/a/3745677/3181933
 function hex2a(hexx) {
   var hex = hexx.toString(); //force conversion
@@ -28,13 +25,11 @@ function hex2a(hexx) {
   return str;
 }
 
-var password = 'Password123';
 // Then get the MD5 hash of these bytes
-var hashed = hex2a(md5(password)); // md5 src: https://stackoverflow.com/a/33486055/7519287
+// var hashed = hex2a(md5(password)); // md5 src: https://stackoverflow.com/a/33486055/7519287
 
-console.log(btoa(hashed));
+// var hashed = console.log(btoa(hashed));
 
-/*
 function hex2a(hexx) {
   var hex = hexx.toString(); //force conversion
   var str = '';
