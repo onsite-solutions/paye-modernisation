af55be39ada416f6fcce6024a38e997c0db2756d
Michael Igoe <michael.igoe@cdetb.ie>
2018-07-20 16:22:12 +0100

    Completed payroll API routes

--- src/server/routes/api/payroll.js
+++ src/server/routes/api/payroll.js
@@ -41,4 +41,58 @@ router.post(
   }
 );
 
+/**
+ * GET api/payroll/checkPayrollSubmission/:payrollRunReference/:submissionId
+ * @desc   Check the current status of a payroll submission
+ * @access Public
+ */
+router.get(
+  '/checkPayrollSubmission/:payrollRunReference/:submissionId',
+  async (req, res) => {
+    await client
+      .get(
+        payroll.checkPayrollSubmission(
+          req.params.payrollRunReference,
+          req.params.submissionId
+        )
+      )
+      .then(response => {
+        res.set('Content-Type', 'text/xml');
+        res
+          .status(200)
+          .send(js2xmlparser.parse('response', JSON.parse(response)));
+      })
+      .catch(err => {
+        if (!res.headersSent) {
+          res.status(err.statusCode || 500).send(err.message);
+        } else {
+          console.log(err);
+        }
+      });
+  }
+);
+
+/**
+ * GET api/payroll/checkPayrollRun/:payrollRunReference
+ * @desc   Check the current status of a payroll run
+ * @access Public
+ */
+router.get('/checkPayrollRun/:payrollRunReference', async (req, res) => {
+  await client
+    .get(payroll.checkPayrollRun(req.params.payrollRunReference))
+    .then(response => {
+      res.set('Content-Type', 'text/xml');
+      res
+        .status(200)
+        .send(js2xmlparser.parse('response', JSON.parse(response)));
+    })
+    .catch(err => {
+      if (!res.headersSent) {
+        res.status(err.statusCode || 500).send(err.message);
+      } else {
+        console.log(err);
+      }
+    });
+});
+
 module.exports = router;
