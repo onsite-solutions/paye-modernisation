cb910b91cbcdde30ead44a55b2f0c9268f5f5d22
Michael Igoe <michael.igoe@cdetb.ie>
2018-07-24 16:14:18 +0100

    LookupPayrollDetailsByReturnPeriod added to schema

---
+++ src/models/LookupPayrollDetailsByReturnPeriodResponse.js
@@ -0,0 +1,121 @@
+//@ts-check
+
+const mongoose = require('mongoose');
+
+/**
+ * Response that lets the employer view a return for a specific period.
+ */
+const LookupPayrollDetailsByReturnPeriodResponseSchema = new mongoose.Schema({
+  employerReg: {
+    type: String,
+    required: true,
+    minLength: 0,
+    maxLength: 100,
+    match: '[0-9]{7,8}[A-Wa-w][A-ITWXZa-itwxz ]?'
+  },
+  agentTAIN: {
+    type: String,
+    match: '[0-9]{5}[A-Wa-w]'
+  },
+  returnPeriod: {
+    periodStartDate: {
+      type: Date,
+      required: true
+    },
+    periodEndDate: {
+      type: Date,
+      required: true
+    }
+  },
+  dateLastUpdated: Date,
+  version: {
+    type: String,
+    minLength: 0,
+    maxLength: 100
+  },
+  periodSummary: {
+    taxOnIncome: {
+      type: Number,
+      required: true
+    },
+    prsi: {
+      type: Number,
+      required: true
+    },
+    usc: {
+      type: Number,
+      required: true
+    },
+    lpt: {
+      type: Number,
+      required: true
+    }
+  },
+  payrollRunDetails: [
+    {
+      payrollRunReference: {
+        type: String,
+        required: true,
+        minLength: 0,
+        maxLength: 50,
+        match: '[A-Za-z0-9_\\-]*'
+      },
+      runDate: {
+        type: Date,
+        required: true
+      },
+      multiPeriod: {
+        type: Boolean,
+        required: true,
+        default: false
+      },
+      taxOnIncome: {
+        type: Number,
+        required: true
+      },
+      prsi: {
+        type: Number,
+        required: true
+      },
+      usc: {
+        type: Number,
+        required: true
+      },
+      lpt: {
+        type: Number,
+        required: true
+      }
+    }
+  ],
+  validationErrors: [
+    {
+      code: {
+        type: String,
+        required: true,
+        minLength: 0,
+        maxLength: 50,
+        match: '[A-Za-z0-9_\\-]*'
+      },
+      path: {
+        type: String,
+        minLength: 0,
+        maxLength: 500
+      },
+      description: {
+        type: String,
+        required: true,
+        minLength: 0,
+        maxLength: 500
+      }
+    }
+  ]
+});
+
+const LookupPayrollDetailsByReturnPeriodResponse = mongoose.model(
+  'LookupPayrollDetailsByReturnPeriodResponse',
+  LookupPayrollDetailsByReturnPeriodResponseSchema
+);
+
+module.exports = {
+  LookupPayrollDetailsByReturnPeriodResponse: LookupPayrollDetailsByReturnPeriodResponse
+};
