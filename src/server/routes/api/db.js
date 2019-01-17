f1d1787ea2cf4ce29f06c7859a02d0c59e3a79c1
Michael Igoe <michael.igoe@cdetb.ie>
2019-01-17 17:12:45 +0000

    Endpoint to update the dateUploaded and employerReference

--- src/server/routes/api/db.js
+++ src/server/routes/api/db.js
@@ -46,4 +46,45 @@ router.post('/rpns/dateInitialised/:fileName', (req, res) => {
   );
 });
 
+/**
+ * POST /api/db/rpns/dateUploaded/ppsn/rpn/empNo
+ * @desc   Update the DateInitialised value for an RPN response file
+ * @access Public
+ */
+router.post('/rpns/dateUploaded/:ppsn/:rpn/:empNo', (req, res) => {
+  RpnResponse.find({
+    'rpns.employeeID.employeePpsn': req.params.ppsn,
+    'rpns.rpnNumber': req.params.rpn
+  }).exec((err, results) => {
+    if (err) {
+      if (!res.headersSent) {
+        res
+          .status(err.statusCode || 500)
+          .send(js2xmlparser.parse('response', JSON.parse(err.message)));
+      } else {
+        console.log(err);
+      }
+    } else {
+      for (let i = 0; i < results.length; i++) {
+        let rpn = new RpnResponse(results[i]);
+
+        for (let j = 0; j < rpn.rpns.length; j++) {
+          // Find any record(s) that contain the relevant RPNs
+          if (
+            rpn.rpns[j].employeeID.employeePpsn == req.params.ppsn &&
+            rpn.rpns[j].rpnNumber == req.params.rpn
+          ) {
+            // Update and save the RPN Response
+            rpn.rpns[j].dateUploaded = Date.now();
+            rpn.rpns[j].employerReference = req.params.empNo;
+            rpn.save();
+          }
+        }
+      }
+
+      res.status(200).send(js2xmlparser.parse('response', 'success'));
+    }
+  });
+});
+
 module.exports = router;
