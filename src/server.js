9e127a8f56ea2b63cbac9bf9c5916e19cca27483
Michael Igoe <michael.igoe@cdetb.ie>
2019-01-23 12:55:56 +0000

    Removed web front end and dependencies

--- src/server.js
+++ src/server.js
@@ -12,18 +12,12 @@ const payroll = require('./server/routes/api/payroll');
 const report = require('./server/routes/api/report');
 const returnsReconciliation = require('./server/routes/api/returns-reconciliation');
 const rpn = require('./server/routes/api/rpn');
-const index = require('./server/index');
-
-const path = require('path');
 
 const app = express();
 
 // Body parser middleware
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: false }));
-
-// Static files
-app.use(express.static(path.join(__dirname, './public')));
 
 // Connect to MongoDB
 mongoose
@@ -42,8 +36,6 @@ app.use('/api/payroll', payroll);
 app.use('/api/report/', report);
 app.use('/api/returns_reconciliation', returnsReconciliation);
 app.use('/api/rpn', rpn);
-
-app.use('/', index);
 
 app.listen(config.port, () =>
   console.log(`Listening at http://localhost:${config.port}/`)
