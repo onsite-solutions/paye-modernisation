042c146606b20ca1ab75b4f803261382888b8f02
Michael Igoe <michael.igoe@cdetb.ie>
2019-01-23 15:55:49 +0000

    chmod fix

--- src/upload.js
+++ src/upload.js
@@ -31,106 +31,128 @@ function getNewRpns() {
       rpnCount: 0
     });
 
+    // Get the most recent to date that is less than the current date
+
     // Get the most recent RpnLogFile in the database
-    RpnFileLog.findOne({}, {}, { sort: { toDate: -1 } }, (err, lastFileLog) => {
-      if (err) {
-        reject(err.message);
-        return;
-      }
+    RpnFileLog.findOne(
+      { toDate: { $lt: today } },
+      {},
+      { sort: { toDate: -1 } },
+      (err, lastFileLog) => {
+        if (err) {
+          reject(err.message);
+          return;
+        }
 
-      if (!validation.isEmpty(lastFileLog)) {
-        // Set the new fromDate to match the most recent toDate
-        newFileLog.fromDate = lastFileLog.toDate;
+        if (!validation.isEmpty(lastFileLog)) {
+          // Set the new fromDate to match the most recent toDate
+          newFileLog.fromDate = lastFileLog.toDate;
 
-        let lastFromDate = moment(lastFileLog.fromDate);
+          let lastFromDate = moment(lastFileLog.fromDate);
 
-        if (lastFromDate.isSame(today)) {
-          // We are rerunning the current day, so remove the earlier file
-          RpnResponse.findOneAndRemove(
-            { fileName: lastFileLog.fileName },
-            err => {
+          if (lastFromDate.isSame(today)) {
+            // We are rerunning the current day, so remove the earlier file
+            RpnResponse.findOneAndRemove(
+              { fileName: lastFileLog.fileName },
+              err => {
+                if (err) {
+                  reject(err.message);
+                  return;
+                }
+              }
+            );
+          }
+        }
+
+        // Get the file contents
+        client
+          .get(
+            rpn.lookUpRpnByEmployer(
+              moment(newFileLog.fromDate).format('YYYY-MM-DD')
+            )
+          )
+          .then(response => {
+            let rpnResponse = new RpnResponse(JSON.parse(response));
+            rpnResponse.fileName = newFileLog.fileName;
+            newFileLog.rpnCount = rpnResponse.totalRPNCount;
+
+            // Save the new RpnFileLog
+            newFileLog.save(err => {
               if (err) {
                 reject(err.message);
                 return;
               }
-            }
-          );
-        }
-      }
-
-      // Get the file contents
-      client
-        .get(
-          rpn.lookUpRpnByEmployer(
-            moment(newFileLog.fromDate).format('YYYY-MM-DD')
-          )
-        )
-        .then(response => {
-          let rpnResponse = new RpnResponse(JSON.parse(response));
-          rpnResponse.fileName = newFileLog.fileName;
-          newFileLog.rpnCount = rpnResponse.totalRPNCount;
-
-          // Save the new RpnFileLog
-          newFileLog.save(err => {
-            if (err) {
-              reject(err.message);
-              return;
-            }
-          });
-
-          // Save the new RpnResponse
-          rpnResponse.save(err => {
-            if (err) {
-              reject(err.message);
-              return;
-            }
-          });
-
-          // Now we need to create the file on the payroll server
-          let xmlBody = js2xmlparser.parse('root', JSON.parse(response));
-          let xmlFileName = `RPN_${newFileLog.fileName}.XML`;
-          let xmlPath = path.join(__dirname, '../src/uploads/') + xmlFileName;
-
-          // Create the xml version of the file on local machine
-          fs.writeFile(xmlPath, xmlBody, err => {
-            if (err) {
-              reject(err.message);
-              return;
-            }
-          });
-
-          // FTP the file to payrolls server
-          let c = new ftp();
-          c.on('ready', function() {
-            c.put(xmlBody, config.ftpDirectory + xmlFileName, function(err) {
-              if (err) throw err;
-              c.end();
             });
-          });
 
-          // connect to payroll server and transfer file
-          c.connect({
-            host: config.ftpHost,
-            user: config.ftpUser,
-            password: config.ftpPassword
-          });
+            // Save the new RpnResponse
+            rpnResponse.save(err => {
+              if (err) {
+                reject(err.message);
+                return;
+              }
+            });
 
-          // Delete the local copy of the file
-          fs.unlink(xmlPath + xmlFileName, err => {
-            if (err) {
-              reject(err.message);
-              return;
+            // Now we need to create the file on the payroll server
+            let xmlBody = js2xmlparser.parse('root', JSON.parse(response));
+            let xmlFileName = `RPN_${newFileLog.fileName}.XML`;
+            let xmlPath = path.join(__dirname, '../src/uploads/') + xmlFileName;
+
+            // Create the xml version of the file on local machine
+            fs.writeFile(xmlPath, xmlBody, err => {
+              if (err) {
+                reject(err.message);
+                return;
+              }
+            });
+
+            // FTP the file to payrolls server
+            let c = new ftp();
+            c.on('ready', function() {
+              c.put(xmlBody, config.ftpDirectory + xmlFileName, function(err) {
+                if (err) throw err;
+                let command = `chmod 666 ${config.ftpDirectory}${xmlFileName}`;
+                c.site(command, (error, response) => {
+                  if (error) {
+                    console.error(error.message);
+                  }
+                });
+                c.end();
+              });
+            });
+
+            // connect to payroll server and transfer file
+            c.connect({
+              host: config.ftpHost,
+              user: config.ftpUser,
+              password: config.ftpPassword
+            });
+
+            // Delete the local copy of the file
+            fs.unlink(xmlPath, err => {
+              if (err) {
+                reject(err.message);
+                return;
+              }
+            });
+
+            // Respond with 'NORPNS' if there are no RPNs on the file, else filename
+            let message = '';
+
+            if (newFileLog.rpnCount == 0) {
+              message = 'NORPNS';
+            } else {
+              message = xmlFileName;
             }
-          });
 
-          resolve(xmlFileName);
-          return;
-        })
-        .catch(err => {
-          reject(err.message);
-          return;
-        });
-    });
+            resolve(message);
+            return;
+          })
+          .catch(err => {
+            reject(err.message);
+            return;
+          });
+      }
+    );
   });
 }
 
